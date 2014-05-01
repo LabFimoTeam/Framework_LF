d6325b81898a1238524521263ba1d7866777fc45
Erik Villegas <erdvillegas@gmail.com>
2014-05-01 08:32:29 -0500

    M Index

--- index.php
+++ index.php
@@ -4,17 +4,8 @@ print
 "<title>Hola</title>
 <h1>Hola mundo </h1>";
 
-print "<h4>Nuevos Cambios</h4>";
-
-print "<h3>Se cambio el archivo index</h3>";
-
-print "<h1>Nuevo cambio</h1>";
-
 print "<h1>Integrando cambios</h1>";
 ?>
-
-<h1>Prueba Jnks</h1>
-
 
 <?php
 require_once('framework/core/lib/myRouter.php');
