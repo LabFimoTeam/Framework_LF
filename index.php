514493282323424c338f44d962e7ea99909972e1
erdvillegas <vi.erik@yahoo.com>
2014-05-03 15:30:16 -0500

    Update

--- index.php
+++ index.php
@@ -4,24 +4,11 @@ print
 "<title>Hola</title>
 <h1>Hola mundo </h1>";
 
-print "<h1>Integrando cambios</h1>";
-?>
-<<<<<<< HEAD
-
-<?php
-require_once('framework/core/lib/myRouter.php');
-=======
-
-<?php
 require_once('./framework/framework.php');
 require_once('framework/core/lib/paths.php');
 
 
-print CONFIGPATH;
-
 $d = new paths();
-
-print "<br/>".LIBPATH;
 
 print "<br/><pre>";
 print_r ($d->get_archivos_ruta(LIBPATH))/
@@ -31,7 +18,6 @@ foreach($d->get_archivos_ruta(LIBPATH) as $k){
 	print "require_once -> ". LIBPATH."$k . . .<br/>";
 	require_once LIBPATH."".$k;
 }
->>>>>>> master
 
 print "<br/><pre>";
 print_r ($d->leer_archivos('./framework/'));
