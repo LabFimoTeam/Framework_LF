fe1dbf05cfc59ac1e1d908d59dc4ed119e7837ff
erdvillegas <vi.erik@yahoo.com>
2014-05-09 01:38:18 -0500

    Implementacion de Boostrap

--- index.php
+++ index.php
@@ -5,18 +5,28 @@ require_once('./framework/framework.php');
 
 //$d = new Librerias\paths();
 
-print "<br/><pre>";
-//print_r ($d->get_archivos_ruta(LIBPATH))/
-print "</pre>";
-
-print "<br/><pre>";
-//print_r ($d->leer_archivos('./framework/'));
-print "</pre>";
 
 
-print STATICPATH;
+/*
+print "<h1>Paths</h1>";
+print "<hr/>".APPATH."<br/><hr/>";
+print "<hr/>".CACHEPATH."<br/><hr/>";
+print "<hr/>".CONTROLADORESPATH."<br/><hr/>";
+print "<hr/>".MODELOSPATH."<br/><hr/>";
+print "<hr/>".STATICPATH."<br/><hr/>";
+print "<hr/>".TEMPLATESPATH."<br/><hr/>";
+
+print "<h1>urls</h1>";
+print "<hr/>".APURL."<br/><hr/>";
+print "<hr/>".CACHEURL."<br/><hr/>";
+print "<hr/>".CONTROLADORESURL."<br/><hr/>";
+print "<hr/>".MODELOSURL."<br/><hr/>";
+print "<hr/>".STATICURL."<br/><hr/>";
+print "<hr/>".TEMPLATESURL."<br/><hr/>";
+*/
+
 $fw = new framework();
 $tg = $fw->Twig("Home", TRUE);
 $tg->loadTemplate('index.html');
-echo $tg->render("index.html",array('titulo' => 'Boostrap', 'STATICPATH' => SERVERURL.STATICPATH));
+echo $tg->render("index.html",array('titulo' => 'Boostrap', 'STATICURL' => STATICURL));
 ?>
